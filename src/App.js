171833560e25a2ce2a84a8b0f404623d4702ec6c
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2021-11-09 14:50:07 +0530

    Update _app.js

--- src/App.js
+++ src/App.js
@@ -1,48 +1,45 @@
-import React, { Component } from 'react'
-import logo from './logo.svg'
-import './App.css'
-
-import { Route, Link, Switch, Redirect } from 'react-router-dom'
-
+import { Helmet } from 'react-helmet'
+import React, { useEffect, useState } from 'react'
+import { Route, Switch, Redirect } from 'react-router-dom'
 import Home from './components/Home'
 import About from './components/About'
+import Header from './components/Header'
 import Messages from './components/Messages'
+import { prefetch } from '@layer0/prefetch/window/prefetch'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <div className="menu">
-          <ul>
-            <li>
-              {' '}
-              <Link to="/">Home</Link>{' '}
-            </li>
-            <li>
-              {' '}
-              <Link to="/messages">Messages</Link>{' '}
-            </li>
-            <li>
-              {' '}
-              <Link to="/about">About</Link>{' '}
-            </li>
-          </ul>
-        </div>
-        <div className="App-intro">
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/messages" component={Messages} />
-            <Route path="/about" component={About} />
-            <Redirect to="/" />
-          </Switch>
-        </div>
-      </div>
-    )
-  }
+const App = () => {
+  const [mounted, setMounted] = useState('print')
+  useEffect(() => {
+    setMounted('all')
+    // register a listener for SW messages to prefetch images from the PLP API responses
+    const { serviceWorker } = navigator
+    if (serviceWorker) {
+      serviceWorker.addEventListener('message', (event) => {
+        if (event.data.action === 'prefetch') {
+          prefetch(event.data.url, event.data.as, event.data.options)
+        }
+      })
+    }
+  }, [])
+
+  return (
+    <>
+      <Helmet>
+        <link
+          media={mounted}
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap"
+        />
+      </Helmet>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        {/* <Route path="/messages" component={Messages} />
+        <Route path="/about" component={About} />
+        <Redirect to="/" /> */}
+      </Switch>
+    </>
+  )
 }
 
 export default App
